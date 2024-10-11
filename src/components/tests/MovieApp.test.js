e23e354ba3de9d14d4e5ab16d239fc0e1876a953
Eduarda Duarte <40005532+eduardadu@users.noreply.github.com>
2024-10-11 01:59:11 +0100

    moves test file (empty - did not had time to make)

--- src/components/tests/MovieApp.test.js
+++ src/components/tests/MovieApp.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import MovieApp from './MovieApp';
+import MovieApp from '../MovieApp';
 
 test('renders learn react link', () => {
   //render(<MovieApp />);
