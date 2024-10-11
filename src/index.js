ef3adcace7bdce350b334d58cc2793776c17e034
Eduarda Duarte <40005532+eduardadu@users.noreply.github.com>
2024-10-11 02:02:12 +0100

    cleaning + removal of strict mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MovieApp from "./components/MovieApp";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MovieApp from './components/MovieApp';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <MovieApp />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MovieApp />);
 
 reportWebVitals();
